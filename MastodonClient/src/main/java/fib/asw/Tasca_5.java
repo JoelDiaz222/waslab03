89cb2e88d2d0e82709f4c227aae8f9f8cb4bd79a
Joel Díaz Álvarez <joel.diaz.alvarez@estudiantat.upc.edu>
2025-02-28 08:55:19 +0100

    tasca #5 finalitzada

--- MastodonClient/src/main/java/fib/asw/Tasca_5.java
+++ MastodonClient/src/main/java/fib/asw/Tasca_5.java
@@ -1,8 +1,47 @@
 package fib.asw;
 
-public class Tasca_5 {
+import org.apache.hc.client5.http.fluent.Request;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ResourceBundle;
+
+public class Tasca_5 {
     public static void main(String[] args) {
-        System.out.println("Encara no faig res!");
+        final String URI_LAST_STATUS = "https://mastodont.cat/api/v1/accounts/109862447110628983/statuses?limit=1";
+        final String URI_BOOST = "https://mastodont.cat/api/v1/statuses/%s/reblog";
+        final String TOKEN = ResourceBundle.getBundle("token").getString("token");
+
+        try {
+            //  GET del tut més recent
+            String output = Request.get(URI_LAST_STATUS)
+                    .addHeader("Authorization","Bearer " + TOKEN)
+                    .execute()
+                    .returnContent()
+                    .asString();
+
+            final JSONArray tuts = new JSONArray(output);
+            final JSONObject lastTut = tuts.getJSONObject(0);
+            final String content = lastTut.getString("content");
+
+            System.out.println("Contingut del tut més recent:");
+            System.out.println(content);
+
+            //  Fem boost al tut
+            final String idLastTut = lastTut.getString("id");
+            final String URL_BOOST_FORMATTED = String.format(URI_BOOST, idLastTut);
+
+            output = Request.post(URL_BOOST_FORMATTED)
+                    .addHeader("Authorization","Bearer " + TOKEN)
+                    .execute()
+                    .returnContent()
+                    .asString();
+
+            System.out.println("Resposta al fer boost d'aquest tut:");
+            System.out.println(output);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
